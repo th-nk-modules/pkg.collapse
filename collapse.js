7a9b461ffe7e5dbb2d722ab5e99711ad7844a047
sion evans <sionevans@gmail.com>
2015-08-13 09:40:16 +0100

    collapse panels

---
+++ collapse.js
@@ -0,0 +1,87 @@
+import {events} from './events';
+import * as dom from './dom';
+/**
+ * @class Collapse
+ * @classdesc JS collapsable panels
+ * accordion, expand, and hover panels
+ */
+class Collapse {
+    /**
+     * @constructor
+     * @param {string} strEvent Event type
+     * @param {Object} containers DOM element
+    */
+    constructor(strEvent, containers) {
+        this.eventAction = strEvent;
+        this.containers = containers || document.querySelectorAll('.collapse');
+        this.elms = [];
+        this.activeElms = [];
+
+        for (var i = 0, il = this.containers.length; i<il; i++){
+            let container = this.containers[i];
+            let type = container.dataset.collapse;
+            dom.addClass(container,type);
+    
+            switch (type){
+                case 'collapse--hover':
+                    break;
+                case 'collapse--expand':
+                    this.expand(container,i);
+                    break;
+                case 'collapse--accordion':
+                    this.accordion(container,i);
+                    break;
+            }
+        }
+    }
+    /**
+     * Expand handler
+     * @memberOf Collapse
+     * @param {object} container Parent .collapse DOM element
+     * @param {number} i Index of parent
+    */
+    expand(container, i) {
+        let elms = container.querySelectorAll('.collapse_heading');
+        this.elms.push(elms);
+
+        for (var i = 0, il = elms.length; i<il; i++){
+            var elm = elms[i];
+
+            events.on(elm,this.eventAction,function() {
+                dom.toggleClass(this,'active');
+            });
+        }
+    }
+    /**
+     * Accordion handler
+     * @memberOf Collapse
+     * @param {object} container Parent .collapse DOM element
+     * @param {number} i Index of parent
+    */
+    accordion(container, i) {
+        let elms = container.querySelectorAll('.collapse_heading');
+        this.elms[i] = elms;
+        this.activeElms[i] = [];
+
+        for (var n = 0, nl = elms.length; n<nl; n++){
+            var elm = elms[n];
+            if (dom.hasClass(elm,'active')) {
+                this.activeElms[i][n] = elm;
+            }
+            var _this = this;
+
+            var clicked = function(ii,nn,elem) {
+                return function() { 
+                    delete _this.activeElms[ii][nn];
+                    _this.activeElms[ii].forEach(el => dom.removeClass(el,'active'))
+                    dom.toggleClass(elem,'active')
+                    _this.activeElms[ii][nn] = elem;
+                }
+            }(i,n,elm)
+
+            events.on(elm,this.eventAction,clicked);
+        }
+    }
+}
+
+export { Collapse };
